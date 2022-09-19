6f661defa2fac22df4fddd67165ff2d92de2cbdb
Vira Murray <vira.avramenko@gmail.com>
2022-09-19 12:49:25 -0400

    exercise "Method overload" complete

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿namespace MethodOverloading
+{
+    internal class Method
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public static decimal Add(decimal a, decimal b)
+        {
+            return a + b;
+        }
+        public static string Add(int a, int b, bool isTrue)
+        {
+            var sum = 0;
+            var response = "";
+
+            if (isTrue)
+            {
+                sum = a + b;
+                if (sum == 1)
+                {
+                    response = $"{sum} dollar";
+                }
+                else
+                {
+                    response = $"{sum} dollars";
+                }
+
+            }
+            return response;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Add(1,1,true));
+            Console.WriteLine();
+            Console.WriteLine(Add(1, 99, true));
+            Console.WriteLine();
+            Console.WriteLine(Add(1,0, true));
+        }
+    }
+}
